95321fa9dd8c99ca323366ea223a93220777d945
Vikas Bajpayee <9716588439vik@gmail.com>
2023-01-24 14:52:43 +0530

    Added powermockito dependency

---
+++ app/src/test/java/com/example/jetpackcompose2022/MovieViewModelTest.kt
@@ -0,0 +1,11 @@
+package com.example.jetpackcompose2022
+
+import com.example.jetpackcompose2022.viewmodel.MovieViewModel
+import org.junit.runner.RunWith
+import org.powermock.modules.junit4.PowerMockRunner
+
+@RunWith(PowerMockRunner::class)
+class MovieViewModelTest {
+
+    val viewModel = MovieViewModel()
+}

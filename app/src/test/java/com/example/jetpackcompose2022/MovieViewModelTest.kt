9627fa24470f10fd4eee900ed8f6f62d072c1aa8
Vikas Bajpayee <9716588439vik@gmail.com>
2023-01-24 14:58:30 +0530

    Added tests for view model class

--- app/src/test/java/com/example/jetpackcompose2022/MovieViewModelTest.kt
+++ app/src/test/java/com/example/jetpackcompose2022/MovieViewModelTest.kt
@@ -1,11 +1,17 @@
 package com.example.jetpackcompose2022
 
+import com.example.jetpackcompose2022.repository.MovieRepository
 import com.example.jetpackcompose2022.viewmodel.MovieViewModel
+import com.nhaarman.mockitokotlin2.mock
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.plus
 import org.junit.runner.RunWith
 import org.powermock.modules.junit4.PowerMockRunner
 
 @RunWith(PowerMockRunner::class)
 class MovieViewModelTest {
-
-    val viewModel = MovieViewModel()
+    private val apiRepository = mock<MovieRepository>()
+    private val coroutineScope = MainScope() + Dispatchers.Unconfined
+    val viewModel = MovieViewModel(apiRepository, coroutineScope)
 }
